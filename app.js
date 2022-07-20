59ed12d5f1392882df414e3c9e001126fc9485bc
ekukwa <easmsm@gmail.com>
2022-07-20 18:59:00 -0400

    found the error in the utility files from the fork, deleted and got inquirer to work

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+console.log(inquirer);
+
+// const generatePage  = require('./src/page-template')
+
+// const pageHTML =generatePage (readme)
+// FileSystem.writeFile('./readme.md', pageHTML, err  => {
+//     if (err) throw err;
+// });
+
+// console.log('ReadMe complete! Check out ReadMe.md to see the output!')
 // TODO: Create an array of questions for user input
 const questions = [];
 
