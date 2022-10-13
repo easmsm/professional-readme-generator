c9b72e5c5f37bf76afc86a35e0910c0c63eb4405
ekukwa <easmsm@gmail.com>
2022-10-12 22:36:23 -0400

    added functions to render badges, license information, table of content and links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,106 @@
-// add badges for licenses
+// functions specific to generating license badges, which will be called in generateMarkdown
 
-// add why/why/how/usage
+//license badge depending on user input
+function renderLicenseBadge(license) { 
+    if (license !== 'no license') {
+      return `
+    ![badge](https://img.shields.io/badge/license-${license}-blue)
+      `;
+    } else {
+      return ' ';
+    }
+  }
 
-//add github username
+  function renderLicenseLink(license) {
+    if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+      `;
+    } else {
+      return ' ';
+    }
+  }
 
-//add email
+  //in the markdown body
+  function renderLicenseSection(license) {
+    if (license !== 'no license') {
+    return `
+    ## [License](#license)
+    The application is covered under the following license:
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+   }
+
+   //table of contents license information
+   function renderLicenseTOC(license) {
+    if (license !== 'no license') {
+    return `
+    * [License](#license)
+      `;
+    } else {
+      return ' ';
+    }
+   }
+  
+
+
+
+// add Title, description, installation, usage, contributing, tests, name, email, GitHub, license
+function generateMarkdown(data) {
+    return `
+        # ${data.title}
+        
+        ${renderLicenseBadge(data.license)}
+
+    ## Table of Contents
+
+        * [Title](#title)
+        * [Description](#description)
+        * [Installation](#installation)
+        * [Usage](#usage)
+        ${renderLicenseTOC(data.license)}
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [Name](#name)
+        * [Contact](#contact)
+
+    ## [Title](#title)
+
+        ${data.description}
+    
+    ## [Installation](#installation)
+
+        ${data.installation}
+    
+    ## [Usage](#usage)
+
+        ${data.usage}
+
+    ${renderLicenseSection(data.license)}
+    
+    ## [Contributing](#contributing)
+
+        ${data.contributing}
+    
+    
+    ## [Tests](#tests)
+
+        ${data.tests}
+
+    ## [Contact](#contact)
+
+        If you have any questions, please contact me using the following links:
+
+        ${data.name}
+
+        [GitHub](https://github.com/${data.githubUsername})
+
+        [Email: ${data.email}](mailto:${data.email})
+    `;
+}
+
+
+module.exports = generateMarkdown;
